d2139a5f47419d71f1ba4c513145106218097f40
surya vishakan <suryavishakan050@gmail.com>
2022-02-17 19:13:54 +0530

    changes made to app.js file

--- js/app.js
+++ js/app.js
@@ -1,26 +1,24 @@
 const countdown = () => {
+  const countDate = new Date("January 1, 2023 00:00:00").getTime();
+  const now = new Date().getTime();
+  const remainingTime = countDate - now;
 
-    const countDate = new Date("July 10, 2021 00:00:00").getTime();
-    const now = new Date().getTime();
-    const remainingTime = countDate - now;
+  /*=======Calculate time=========*/
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    /*=======Calculate time=========*/
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  const displayDay = Math.floor(remainingTime / day);
+  const displayHour = Math.floor((remainingTime % day) / hour);
+  const displayMinute = Math.floor((remainingTime % hour) / minute);
+  const displaySecond = Math.floor((remainingTime % minute) / second);
 
-    const displayDay = Math.floor( remainingTime / day);
-    const displayHour = Math.floor((remainingTime % day) / hour);
-    const displayMinute = Math.floor((remainingTime % hour) / minute);
-    const displaySecond = Math.floor((remainingTime % minute) / second);
+  /*========Update HTML and Display Time=======*/
+  document.querySelector(".day").innerText = displayDay;
+  document.querySelector(".hour").innerText = displayHour;
+  document.querySelector(".minute").innerText = displayMinute;
+  document.querySelector(".second").innerText = displaySecond;
+};
 
-    /*========Update HTML and Display Time=======*/
-    document.querySelector(".day").innerText = displayDay;
-    document.querySelector(".hour").innerText = displayHour;
-    document.querySelector(".minute").innerText = displayMinute;
-    document.querySelector(".second").innerText = displaySecond;
-
-}
-
-setInterval(countdown,1000);
+setInterval(countdown, 1000);
